1ab0c532275fc6245388f8649b3c08733ddba0c0
Stratos Provatopoulos <strapro@gmail.com>
2022-12-09 20:02:17 +0200

    Added material design icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     shim: false,
   },
   modules: ['@nuxtjs/supabase'],
-  css: ['vuetify/lib/styles/main.sass'],
+  css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
   build: {
     transpile: ['vuetify'],
   },

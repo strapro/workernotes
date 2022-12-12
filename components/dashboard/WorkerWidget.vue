9bb27700f95b2b3fe665e4d4386d2a820b282e3c
Stratos Provatopoulos <strapro@gmail.com>
2022-12-12 21:56:17 +0200

    Added control to add note to worker from dashboard

--- components/dashboard/WorkerWidget.vue
+++ components/dashboard/WorkerWidget.vue
@@ -5,12 +5,15 @@
     <v-card-title>{{ worker.first_name }} {{ worker.last_name }}</v-card-title>
 
     <v-card-text>
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
+      <v-textarea v-model="note" auto-grow variant="filled" color="deep-purple" label="Note" rows="1"></v-textarea>
     </v-card-text>
 
     <v-divider class="mx-4"></v-divider>
 
-    <v-card-actions> </v-card-actions>
+    <v-card-actions class="justify-end">
+      <v-btn color="warning" elevation="2" icon="mdi-thumb-down-outline" @click="createNote(-1)"></v-btn>
+      <v-btn color="primary" elevation="2" icon="mdi-thumb-up-outline" @click="createNote(1)"></v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
@@ -23,9 +26,36 @@ const props = defineProps<{
   worker: Worker;
 }>();
 
+const emit = defineEmits<{
+  (e: 'workerNoteCreated'): void;
+}>();
+
 const supabase = useSupabaseClient<Database>();
+
+const { $uuid } = useNuxtApp();
 
 const profilePic = props.worker.profile_pic
   ? supabase.storage.from('worker-profile-pics').getPublicUrl(props.worker.profile_pic).data.publicUrl
   : 'no-pic.png';
+
+const note = ref('');
+
+const createNote = async (sentiment: Number) => {
+  const { data, error } = await supabase.from('worker_notes').insert([
+    {
+      id: $uuid(),
+      worker_id: props.worker.id,
+      note: note.value,
+      sentiment: sentiment,
+    },
+  ]);
+
+  if (!error) {
+    emit('workerNoteCreated');
+
+    return data;
+  }
+
+  return null;
+};
 </script>

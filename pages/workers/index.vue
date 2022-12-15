3642cb025a4177697ebe0dbb40fadcada1b94821
Stratos Provatopoulos <strapro@gmail.com>
2022-12-15 20:44:56 +0200

    Add workers page with data table

---
+++ pages/workers/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-sheet elevation="1">
+    <EasyDataTable :headers="headers" :items="workers" alternating>
+      <template #item-pic="{ profile_pic }">
+        <v-avatar>
+          <v-img :src="$workerPicPuclicUrl(profile_pic)" alt="John"></v-img>
+        </v-avatar>
+      </template>
+      <template #item-actions="item">
+        <v-btn :to="`/workers/${item.id}`" icon="mdi-magnify" size="x-small" class="my-2"></v-btn>
+      </template>
+    </EasyDataTable>
+  </v-sheet>
+</template>
+
+<script lang="ts" setup>
+import { Database } from 'types/database';
+import type { Header } from 'vue3-easy-data-table';
+
+const supabase = useSupabaseClient<Database>();
+const user = useSupabaseUser();
+
+const headers: Header[] = [
+  { text: '', value: 'pic', width: 60 },
+  { text: 'First name', value: 'first_name' },
+  { text: 'Last name', value: 'last_name' },
+  { text: 'Actions', value: 'actions', width: 100 },
+];
+
+const { data: workers, refresh: refreshWorkers } = await useAsyncData('workers', async () => {
+  if (user.value) {
+    const { data, error } = await supabase.from('workers').select('*').eq('manager_id', user.value.id);
+
+    if (!error) {
+      return data;
+    }
+  }
+
+  return null;
+});
+
+definePageMeta({
+  middleware: 'auth',
+  layout: 'app',
+});
+</script>
